e272bf2bc6e719b4fa189826ba0147b983d76beb
Admin <mirror50s@mail.ru>
2022-10-06 23:45:15 +0300

    uodate elements tests added test_radio_button

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -4,7 +4,7 @@ import time
 from selenium.webdriver.common.by import By
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_page import BasePage
 
 class TextBoxPage(BasePage):
@@ -85,3 +85,13 @@ class CheckBoxPage(BasePage):
 #         for item in result_list:
 #             data.append(item.text)
 #         return data
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+    def click_on_the_radio_button(self, choice): # добавил аргумент choise для выбора 3 радиобаттонов
+        choices = {'yes': self.locators.YES_RADIOBUTTON,
+                  'impressive': self.locators.IMPRESSIVE_RADIOBUTTON,
+                  'no': self.locators.NO_RADIOBUTTON}
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
